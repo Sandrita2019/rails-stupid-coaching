838d3039b420de0cedd0b1252fb32099c30ef588
Sandra Porqueres <sandraporqueres@gmail.com>
2020-08-10 16:28:43 +0200

    last part

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,10 @@
 class QuestionsController < ApplicationController
   def ask
   end
+  def answer
+# recuperer input soumis dans le form (dans l variable paramas hash)
+# en fonction de lìnput, que repond le coach  if/else statement
+# une fois la reponse trouve, la view doit l´afficher  (envoyer une info dans rails du controller a la view)
+  end
 end
+#
